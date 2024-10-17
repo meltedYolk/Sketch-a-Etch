29c26c8adb5a5efcf5b4f90b5a1125f844c13694
meltedyolk <181055160+meltedYolk@users.noreply.github.com>
2024-10-17 11:35:50 -0700

    Add footer and header to page Add user input on grid length Add reset button
    user can select personalized grid and reset
    without refreshing pages

--- js_file.js
+++ js_file.js
@@ -1,8 +1,11 @@
-GRID_LENGTH = 16;
-mouseHeldDown = false;
+let GRID_LENGTH, CARD_HEIGHT, FLEX_PERCENT;
+let mouseHeldDown = false;
+const CONTAINER_WIDTH = 600; // px, need to manually set = to css container width
+const MAX_GRID_LENGTH = 100;
 
-// creates lengthxlength grid 
+// creates lengthxlength grid, restricted to container width
 // cards: attach to container, hover event changes color,
+// intialize reset button
 function createGrid() {
     let i, j;
     const container = document.querySelector(".container")
@@ -10,6 +13,8 @@ function createGrid() {
         for(j = 0; j < GRID_LENGTH; ++j) {
             const card = document.createElement("div");
             card.classList.add("card");
+            card.style.height = CARD_HEIGHT + "px";
+            card.style.flex = "1 0 " + FLEX_PERCENT + "%";
             card.addEventListener("mouseover", () => {
                 if (mouseHeldDown)
                     card.style.backgroundColor = "blue";
@@ -23,6 +28,25 @@ function createGrid() {
             container.appendChild(card);
         }
     }
+    intializeResetBtn();
 }
 
+// user inputs grid length, calc card's height and flex
+function promptGridLength() {
+    do {
+        GRID_LENGTH = parseInt(prompt("choose a grid length 1-100", 16));
+        CARD_HEIGHT = CONTAINER_WIDTH / GRID_LENGTH;
+        FLEX_PERCENT = (CARD_HEIGHT / CONTAINER_WIDTH) * 100;
+    } while(GRID_LENGTH > MAX_GRID_LENGTH);
+}
+
+function intializeResetBtn() {
+    const resetBtn = document.querySelector(".reset");
+    resetBtn.addEventListener("click", () => {
+        let cards = document.querySelectorAll(".card");
+        cards.forEach((card) => {card.style.backgroundColor = "whitesmoke";});
+    });
+}
+
+promptGridLength();
 createGrid();
